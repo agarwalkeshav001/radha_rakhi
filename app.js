6f0dd5d2a96c5a64e7773065fdf19d8d6be123a6
agarwalkeshav001 <keshavagrwl123@gmail.com>
2020-05-20 16:33:14 +0530

    port change

--- app.js
+++ app.js
@@ -767,7 +767,7 @@ app.get('/logout', (req, res) => {
     }
 });
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 80
 app.listen(port, () => {
     console.log(`Server live at port: ${port}`)
 })
